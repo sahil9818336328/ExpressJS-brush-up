88334e8fc5e1c7553f03e596643f66822c88c0bd
Sahil khatri <Sahilkehsav1997@gmail.com>
2023-08-25 17:39:21 +0530

    express router

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const people = require('./routes/people')
+const auth = require('./routes/login')
+
+// parse json
+app.use(express.json())
+
+app.use('/api/people', people)
+app.use('/login', auth)
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
